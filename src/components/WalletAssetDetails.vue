63b467d87bda48ceb6e3ad3389ee1e2de515a7dd
Stefan Popov <stefanpopov@list.ru>
2020-11-11 01:35:12 +0300

    Implement Wallet Asset Details component

---
+++ src/components/WalletAssetDetails.vue
@@ -0,0 +1,143 @@
+<template>
+  <wallet-base :title="currentRouteParams.symbol" show-back show-settings @back="handleBack">
+    <div v-if="selectedAssetDetails.length > 0" class="asset-details s-flex">
+      <div
+        class="asset-details-item s-flex"
+        v-for="(item, index) in selectedAssetDetails"
+        :key="`asset-details-${index}`"
+      >
+        <div class="info s-flex">
+          <div class="info-operation">{{ item.operation }}</div>
+          <div class="info-text">
+            <span>{{ `${item.fromAmount} ${currentRouteParams.symbol} for ${item.toAmount} ${item.toSymbol}` }}</span>
+            <s-button
+              class="info-text_explorer"
+              type="link"
+              size="small"
+              @click="handleOpenBlockExplorer(item)"
+            >
+              <s-icon name="external-link" size="16px" />
+            </s-button>
+          </div>
+          <s-icon :class="getStatusClass(item.status)" :name="getStatusName(item.status)" size="20px" />
+        </div>
+        <div class="date">{{ formatDate(item.date) }}</div>
+      </div>
+    </div>
+    <div v-else class="asset-details-empty">{{ t('assets.emptyDetails') }}</div>
+  </wallet-base>
+</template>
+
+<script lang="ts">
+import { Component, Mixins, Prop } from 'vue-property-decorator'
+import { Action, Getter } from 'vuex-class'
+
+import TranslationMixin from './mixins/TranslationMixin'
+import WalletBase from './WalletBase.vue'
+import { RouteNames } from '../consts'
+import { formatDate } from '../util'
+
+@Component({
+  components: { WalletBase }
+})
+export default class WalletAssetDetails extends Mixins(TranslationMixin) {
+  @Getter currentRouteParams!: any
+  @Getter selectedAssetDetails!: Array<any>
+  @Action navigate
+  @Action getAssetDetails
+
+  mounted (): void {
+    this.getAssetDetails({ symbol: this.currentRouteParams.symbol })
+  }
+
+  handleBack (): void {
+    this.navigate({ name: RouteNames.Wallet })
+  }
+
+  handleOpenBlockExplorer (item: any): void {
+    this.$emit('block-explorer', item)
+  }
+
+  formatDate (value: number): string {
+    return formatDate(value)
+  }
+
+  getStatusName (status: string): string {
+    switch (status) {
+      case 'IN_PROGRESS': return 'refresh'
+      case 'ERROR': return 'circle-x'
+      case 'SUCCESS': return 'check-mark'
+    }
+    return ''
+  }
+
+  getStatusClass (status: string): string {
+    let state = ''
+    switch (status) {
+      case 'IN_PROGRESS':
+        state = 'loading'
+        break
+      case 'ERROR':
+        state = 'error'
+        break
+      case 'SUCCESS':
+        state = 'success'
+        break
+    }
+    return state ? `info-status info-status--${state}` : 'info-status'
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import '../styles/soramitsu-variables';
+@import '../styles/typography';
+@import '../styles/layout';
+
+.asset-details {
+  flex-direction: column;
+  &-item {
+    flex-direction: column;
+    .info {
+      align-items: center;
+      &-operation {
+        color: $s-color-base-content-secondary;
+        background-color: $s-color-base-background;
+        border-radius: $border-radius;
+        font-size: $font-size_mini;
+        font-weight: bold;
+        padding: 4px;
+        margin-right: $basic-spacing_mini;
+      }
+      &-text {
+        flex: 1;
+        font-size: $font-size_small;
+        > span {
+          line-height: 3.4;
+        }
+        &_explorer {
+          margin-left: $basic-spacing_mini;
+          padding: 0;
+        }
+      }
+      &-status {
+        &--success {
+          color: $s-color-status-success;
+        }
+        &--error {
+          color: $s-color-status-error;
+        }
+      }
+    }
+    .date {
+      font-size: $font-size_small;
+      color: $s-color-base-content-tertiary;
+    }
+  }
+  &-empty {
+    text-align: center;
+    font-size: $font-size_small;
+    color: $s-color-base-content-tertiary;
+  }
+}
+</style>
