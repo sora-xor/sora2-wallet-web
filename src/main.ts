d8fb7c66c2e0e0625aa97acc3358c65086272d20
Stefan Popov <stefanpopov@list.ru>
2021-01-21 13:59:04 +0400

    Update connection

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
+// This file is only for local usage
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import i18n from './lang'
-import { dexApi } from './api'
+import { connection } from './api'
 import * as env from '../public/env.json'
 
 import './plugins'
 import './styles'
 
-dexApi.endpoint = env.BLOCKCHAIN_URL
+connection.endpoint = env.BLOCKCHAIN_URL
 
 Vue.config.productionTip = false
 Vue.config.devtools = process.env.NODE_ENV === 'development'
