1157ff95017b581c43638f042673e501295ef857
Stefan Popov <stefanpopov@list.ru>
2021-03-21 17:30:43 +0300

    Fix build issues (#129)

--- src/plugins/soramitsuUI.ts
+++ src/plugins/soramitsuUI.ts
@@ -2,24 +2,23 @@ import Vue from 'vue'
 import SoramitsuElements, { Message, MessageBox, Notification, setTheme } from '@soramitsu/soramitsu-js-ui'
 import '@soramitsu/soramitsu-js-ui/lib/styles'
 
-import store from '../store'
-
-Vue.use(SoramitsuElements, { store })
-
-setTheme('light')
-Vue.prototype.$prompt = MessageBox.prompt
-Vue.prototype.$alert = MessageBox.alert
-Vue.prototype.$message = Message
-Vue.prototype.$notify = ({ message, type }) => {
-  Notification({
-    message,
-    title: '',
-    duration: 4500, // If is will be changed you should change animation duration as well
-    type,
-    customClass: 'sora s-flex'
-  })
-  const el = document.createElement('div')
-  el.className = 'loader'
-  const elements = Array.from(document.getElementsByClassName('el-notification'))
-  elements[elements.length - 1].appendChild(el)
+export function install (store) {
+  Vue.use(SoramitsuElements, { store })
+  setTheme('light')
+  Vue.prototype.$prompt = MessageBox.prompt
+  Vue.prototype.$alert = MessageBox.alert
+  Vue.prototype.$message = Message
+  Vue.prototype.$notify = ({ message, type }) => {
+    Notification({
+      message,
+      title: '',
+      duration: 4500, // If is will be changed you should change animation duration as well
+      type,
+      customClass: 'sora s-flex'
+    })
+    const el = document.createElement('div')
+    el.className = 'loader'
+    const elements = Array.from(document.getElementsByClassName('el-notification'))
+    elements[elements.length - 1].appendChild(el)
+  }
 }
