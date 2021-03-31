19be2d661fb224d507cef6f4d0960c40fad15c23
Stefan Popov <stefanpopov@list.ru>
2021-03-31 13:14:15 +0300

    Balances and asset name improvements (#152)
    * Improve asset name and balances updates
    
    * Update index.ts
    
    * Update README.md

--- src/lang/en/index.ts
+++ src/lang/en/index.ts
@@ -1,4 +1,4 @@
-import { KnownSymbols, Operation, TransactionStatus } from '@sora-substrate/util'
+import { Operation, TransactionStatus } from '@sora-substrate/util'
 
 import { AddAssetTabs, WalletTabs } from '../../consts'
 
@@ -37,11 +37,6 @@ export default {
       [Operation.Transfer]: 'Failed to send {amount} {symbol} to {address}',
       [Operation.RegisterAsset]: 'Failed to register {symbol} asset'
     }
-  },
-  assetNames: {
-    [KnownSymbols.XOR]: 'SORA',
-    [KnownSymbols.VAL]: 'SORA Validator Token',
-    [KnownSymbols.PSWAP]: 'Polkaswap'
   },
   polkadotjs: {
     noExtensions: 'No Polkadot.js extension was found. Please install it and reload this page\nhttps://polkadot.js.org/extension/',
@@ -142,6 +137,10 @@ export default {
     tokenSymbol: {
       placeholder: 'Token symbol',
       desc: 'This is an identifier for the token. You can use from 1 to 7 characters for the selection.'
+    },
+    tokenName: {
+      placeholder: 'Token name',
+      desc: 'This is a name for the token. You can use from 1 to 33 characters for the selection.'
     },
     tokenSupply: {
       placeholder: 'Token supply',
