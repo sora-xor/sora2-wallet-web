65dde4752bf7d970cce39ca0c5f4976b1e8a694c
alexnatalia <alekseenko@soramitsu.co.jp>
2021-07-22 09:21:38 +0300

    Updated tests.

--- tests/unit/components/FiatValue.spec.ts
+++ tests/unit/components/FiatValue.spec.ts
@@ -1,15 +1,18 @@
 import { shallowMount } from '@vue/test-utils'
 
 import { useDescribe, localVue } from '../../utils'
+import { MOCK_FIAT_VALUES } from '../../utils/mock'
+
 import FiatValue from '@/components/FiatValue.vue'
 
 useDescribe('FiatValue.vue', FiatValue, () => {
-  it('should be rendered correctly', () => {
+  MOCK_FIAT_VALUES.map(item => it(`[${item.title}]: should be rendered correctly`, () => {
     const propsData = {
-      value: '1234.5678',
-      withDecimals: true
+      value: item.value,
+      withDecimals: item.withDecimals,
+      withLeftShift: item.withLeftShift
     }
     const wrapper = shallowMount(FiatValue, { localVue, propsData })
     expect(wrapper.element).toMatchSnapshot()
-  })
+  }))
 })
