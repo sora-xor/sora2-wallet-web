982089b3b1083c675507c659c545d057edd33730
Stefan Popov <stefanpopov@list.ru>
2021-07-23 12:37:21 +0400

    Update ui lib version

--- src/plugins/soramitsuUI.ts
+++ src/plugins/soramitsuUI.ts
@@ -1,28 +1,29 @@
 import Vue from 'vue'
 
-import { Theme } from '@soramitsu/soramitsu-js-ui/src/utils/Theme'
-import { DesignSystem } from '@soramitsu/soramitsu-js-ui/src/utils/DesignSystem'
-import { setTheme, setDesignSystem } from '@soramitsu/soramitsu-js-ui/src/utils'
+import Theme from '@soramitsu/soramitsu-js-ui/lib/types/Theme'
+import DesignSystem from '@soramitsu/soramitsu-js-ui/lib/types/DesignSystem'
+import { setTheme, setDesignSystem } from '@soramitsu/soramitsu-js-ui/lib/utils'
 
-import ElementUIPlugin, { Message, MessageBox, Notification } from '@soramitsu/soramitsu-js-ui/src/plugins/elementUI'
-import SoramitsuUIStorePlugin from '@soramitsu/soramitsu-js-ui/src/plugins/soramitsuUIStore'
+import ElementUIPlugin, { Message, MessageBox, Notification } from '@soramitsu/soramitsu-js-ui/lib/plugins/elementUI'
+import SoramitsuUIStorePlugin from '@soramitsu/soramitsu-js-ui/lib/plugins/soramitsuUIStore'
 
-import SButton from '@soramitsu/soramitsu-js-ui/src/components/Button/SButton'
-import SCard from '@soramitsu/soramitsu-js-ui/src/components/Card/SCard'
-import SDesignSystemProvider from '@soramitsu/soramitsu-js-ui/src/components/DesignSystem/SDesignSystemProvider'
-import SDivider from '@soramitsu/soramitsu-js-ui/src/components/Divider/SDivider'
-import SDropdown from '@soramitsu/soramitsu-js-ui/src/components/Dropdown/SDropdown'
-import SDropdownItem from '@soramitsu/soramitsu-js-ui/src/components/Dropdown/SDropdownItem'
-import SIcon from '@soramitsu/soramitsu-js-ui/src/components/Icon/SIcon'
-import SInput from '@soramitsu/soramitsu-js-ui/src/components/Input/SInput'
-import SFloatInput from '@soramitsu/soramitsu-js-ui/src/components/Input/SFloatInput'
-import SForm from '@soramitsu/soramitsu-js-ui/src/components/Form/SForm'
-import SFormItem from '@soramitsu/soramitsu-js-ui/src/components/Form/SFormItem'
-import SPagination from '@soramitsu/soramitsu-js-ui/src/components/Pagination'
-import SSwitch from '@soramitsu/soramitsu-js-ui/src/components/Switch'
-import STab from '@soramitsu/soramitsu-js-ui/src/components/Tab/STab'
-import STabs from '@soramitsu/soramitsu-js-ui/src/components/Tab/STabs'
-import STooltip from '@soramitsu/soramitsu-js-ui/src/components/Tooltip'
+import SButton from '@soramitsu/soramitsu-js-ui/lib/components/Button/SButton'
+import SCard from '@soramitsu/soramitsu-js-ui/lib/components/Card/SCard'
+import SDesignSystemProvider from '@soramitsu/soramitsu-js-ui/lib/components/DesignSystem/SDesignSystemProvider'
+import SDivider from '@soramitsu/soramitsu-js-ui/lib/components/Divider/SDivider'
+import SDropdown from '@soramitsu/soramitsu-js-ui/lib/components/Dropdown/SDropdown'
+import SDropdownItem from '@soramitsu/soramitsu-js-ui/lib/components/Dropdown/SDropdownItem'
+import SIcon from '@soramitsu/soramitsu-js-ui/lib/components/Icon/SIcon'
+import SInput from '@soramitsu/soramitsu-js-ui/lib/components/Input/SInput'
+import SFloatInput from '@soramitsu/soramitsu-js-ui/lib/components/Input/SFloatInput'
+import SForm from '@soramitsu/soramitsu-js-ui/lib/components/Form/SForm'
+import SFormItem from '@soramitsu/soramitsu-js-ui/lib/components/Form/SFormItem'
+import SPagination from '@soramitsu/soramitsu-js-ui/lib/components/Pagination'
+import SScrollbar from '@soramitsu/soramitsu-js-ui/lib/components/Scrollbar'
+import SSwitch from '@soramitsu/soramitsu-js-ui/lib/components/Switch'
+import STab from '@soramitsu/soramitsu-js-ui/lib/components/Tab/STab'
+import STabs from '@soramitsu/soramitsu-js-ui/lib/components/Tab/STabs'
+import STooltip from '@soramitsu/soramitsu-js-ui/lib/components/Tooltip'
 
 export function install (vue: typeof Vue, store) {
   vue.use(ElementUIPlugin)
@@ -39,6 +40,7 @@ export function install (vue: typeof Vue, store) {
   vue.use(SForm)
   vue.use(SFormItem)
   vue.use(SPagination)
+  vue.use(SScrollbar)
   vue.use(SSwitch)
   vue.use(STab)
   vue.use(STabs)
