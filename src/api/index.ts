5f8e2c5bc92a26cc1a64b5b186136d58facc1aa8
Stefan Popov <stefanpopov@list.ru>
2021-07-27 17:37:25 +0400

    Change axios config

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,6 +4,9 @@ import { api, connection } from '@sora-substrate/util'
 import { storage } from '../util/storage'
 
 const axios = axiosInstance.create()
+
+axios.defaults.headers.common['Cache-Control'] = 'no-cache'
+
 api.setStorage(storage)
 
 export {
