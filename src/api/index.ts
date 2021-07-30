7f352034507def045af4acda7e0572d76fb2662e
Nikita-Polyakov <polyakov@soramitsu.co.jp>
2021-07-30 10:53:56 +0300

    fix axios baseUrl

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,8 +4,11 @@ import { api, connection } from '@sora-substrate/util'
 import { storage } from '../util/storage'
 
 const axios = axiosInstance.create()
+const { origin, pathname } = window.location
+const baseUrl = `${origin}${pathname}`
 
 axios.defaults.headers.common['Cache-Control'] = 'no-cache'
+axios.defaults.baseURL = baseUrl
 
 api.setStorage(storage)
 
