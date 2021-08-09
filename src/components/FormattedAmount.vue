98fd635e692e3481e2f3dacddc4581661d17ba1d
alexnatalia <alekseenko@soramitsu.co.jp>
2021-08-09 19:12:03 +0300

    Added comments to properties.

--- src/components/FormattedAmount.vue
+++ src/components/FormattedAmount.vue
@@ -25,6 +25,9 @@ interface FormattedAmountValues {
 
 @Component
 export default class FormattedAmount extends Mixins(NumberFormatterMixin) {
+  /**
+   * Balance or Amount value.
+   */
   @Prop({ default: '', type: String }) readonly value!: string
   /**
    * Font size rate between integer and decimal numbers' parts. Possible values: `"small"`, `"medium"`, `"normal"`.
@@ -36,10 +39,26 @@ export default class FormattedAmount extends Mixins(NumberFormatterMixin) {
    * By default it's set to `"normal"` and it means the same font weights for both numbers' parts.
    */
   @Prop({ default: FontWeightRate.NORMAL, type: String }) readonly fontWeightRate?: string
+  /**
+   * Amount value's asset symbol.
+   */
   @Prop({ default: '', type: String }) readonly assetSymbol?: string
+  /**
+   * Font size of asset symbol is the same with decimal part size.
+   * Symbol value located inside formatted-amount__decimal container at the HTML structure.
+   */
   @Prop({ default: false, type: Boolean }) readonly symbolAsDecimal?: boolean
+  /**
+   * Adds special class and styles to formatted number to convert in to Fiat value.
+   */
   @Prop({ default: false, type: Boolean }) readonly isFiatValue?: boolean
+  /**
+   * Fills only intger part if we don't need decimals value.
+   */
   @Prop({ default: false, type: Boolean }) readonly integerOnly?: boolean
+  /**
+   * Added special class to left shifting for Fiat value if needed (the shift is the same in all screens).
+   */
   @Prop({ default: false, type: Boolean }) readonly withLeftShift?: boolean
 
   get unformatted (): string {
--- src/components/FormattedAmountWithFiatValue.vue
+++ src/components/FormattedAmountWithFiatValue.vue
@@ -33,17 +33,58 @@ import FormattedAmount from './FormattedAmount.vue'
   }
 })
 export default class FormattedAmountWithFiatValue extends Vue {
+  /**
+   * Amount value's custom class.
+   */
   @Prop({ default: '', type: String }) readonly valueClass?: string
+  /**
+   * Balance or Amount value.
+   */
   @Prop({ default: '', type: String }) readonly value!: string
+  /**
+   * Amount value's Font size rate between integer and decimal numbers' parts. Possible values: `"small"`, `"medium"`, `"normal"`.
+   * By default it's set to `"normal"` and it means the same font sizes for both numbers' parts.
+   */
   @Prop({ default: FontSizeRate.NORMAL, type: String }) readonly fontSizeRate?: string
+  /**
+   * Amount value's Font weight rate between integer and decimal numbers' parts. Possible values: `"small"`, `"medium"`, `"normal"`.
+   * By default it's set to `"normal"` and it means the same font weights for both numbers' parts.
+   */
   @Prop({ default: FontWeightRate.NORMAL, type: String }) readonly fontWeightRate?: string
+  /**
+   * Amount value's asset symbol.
+   */
   @Prop({ default: '', type: String }) readonly assetSymbol?: string
+  /**
+   * Font size of asset symbol is the same with decimal part size for Amount value.
+   * Symbol value located inside formatted-amount__decimal container at the HTML structure.
+   */
   @Prop({ default: false, type: Boolean }) readonly symbolAsDecimal?: boolean
+  /**
+   * Sometimes presence of Fiat value related on some rules. With this setting we could show/hide Fiat value part.
+   */
   @Prop({ default: true, type: Boolean }) readonly hasFiatValue?: boolean
+  /**
+   * Fiat value.
+   */
   @Prop({ default: '', type: String }) readonly fiatValue?: string
+  /**
+   * We could set this flag to true if we have the same FontSizeRate and FontWeightRate with Amount value.
+   */
   @Prop({ default: false, type: Boolean }) readonly fiatFormatAsValue?: boolean
+  /**
+   * Fiat value's Font size rate between integer and decimal numbers' parts. Possible values: `"small"`, `"medium"`, `"normal"`.
+   * By default it's set to `"normal"` and it means the same font sizes for both numbers' parts.
+   */
   @Prop({ default: FontSizeRate.NORMAL, type: String }) readonly fiatFontSizeRate?: string
+  /**
+   * Fiat value's Font weight rate between integer and decimal numbers' parts. Possible values: `"small"`, `"medium"`, `"normal"`.
+   * By default it's set to `"normal"` and it means the same font weights for both numbers' parts.
+   */
   @Prop({ default: FontWeightRate.NORMAL, type: String }) readonly fiatFontWeightRate?: string
+  /**
+   * Added special class to left shifting for Fiat value if needed (the shift is the same in all screens).
+   */
   @Prop({ default: false, type: Boolean }) readonly withLeftShift?: boolean
 }
 </script>
