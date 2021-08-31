eeabfee647a3de23acd72aa4475dd1abb8c3a8ba
alexnatalia <alekseenko@soramitsu.co.jp>
2021-09-01 00:50:59 +0300

    Unit tests: Added CreateToken component test.

---
+++ tests/unit/components/CreateToken.spec.ts
@@ -0,0 +1,26 @@
+import { shallowMount } from '@vue/test-utils'
+
+import { useDescribe, localVue } from '../../utils'
+import { MOCK_CREATE_TOKEN } from '../../utils/CreateTokenMock'
+
+import CreateToken from '@/components/CreateToken.vue'
+
+useDescribe('CreateToken.vue', CreateToken, () => {
+  MOCK_CREATE_TOKEN.map(item => it(`[${item.title}]: should be rendered correctly`, () => {
+    const wrapper = shallowMount(CreateToken, {
+      localVue,
+      mocks: { $t: () => {} },
+      data () {
+        return {
+          step: item.step,
+          tokenSymbol: item.tokenSymbol,
+          tokenName: item.tokenName,
+          tokenSupply: item.tokenSupply,
+          extensibleSupply: item.extensibleSupply,
+          hasEnoughXor: item.hasEnoughXor
+        }
+      }
+    })
+    expect(wrapper.element).toMatchSnapshot()
+  }))
+})
