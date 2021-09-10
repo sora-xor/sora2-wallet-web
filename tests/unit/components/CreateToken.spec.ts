866c8edc2ed1486196c641b2ec49a26884eb38af
Nikita-Polyakov <polyakov@soramitsu.co.jp>
2021-09-10 12:44:52 +0300

    update test

--- tests/unit/components/CreateToken.spec.ts
+++ tests/unit/components/CreateToken.spec.ts
@@ -1,14 +1,27 @@
+import Vuex from 'vuex'
 import { shallowMount } from '@vue/test-utils'
 
 import { useDescribe, localVue } from '../../utils'
 import { MOCK_CREATE_TOKEN } from '../../utils/CreateTokenMock'
 
 import CreateToken from '@/components/CreateToken.vue'
+import { MOCK_NETWORK_FEES } from '../../utils/NetworkFeesMock'
+
+const createStore = () => new Vuex.Store({
+  modules: {
+    Settings: {
+      getters: {
+        networkFees: () => MOCK_NETWORK_FEES
+      }
+    }
+  } as any
+})
 
 useDescribe('CreateToken.vue', CreateToken, () => {
   MOCK_CREATE_TOKEN.map(item => it(`[${item.title}]: should be rendered correctly`, () => {
     const wrapper = shallowMount(CreateToken, {
       localVue,
+      store: createStore(),
       data () {
         return {
           step: item.step,
