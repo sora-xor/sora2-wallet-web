23ae4608712a8075ef290ca26991d9b1858f2c1a
Stefan Popov <stefanpopov@list.ru>
2021-09-28 15:31:43 +0400

    Update dependencies (#436)

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,6 +30,7 @@ module.exports = {
     '@typescript-eslint/no-use-before-define': ['off'],
     '@typescript-eslint/no-unused-vars': ['off'],
     '@typescript-eslint/no-empty-function': ['off'],
+    '@typescript-eslint/no-var-requires': ['off'],
   },
   overrides: [
     {
