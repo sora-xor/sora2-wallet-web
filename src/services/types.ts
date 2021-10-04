e0a35a2fef0b5b95e1c3df3a836222ad7c011316
Nikita Polyakov <53777036+Nikita-Polyakov@users.noreply.github.com>
2021-10-04 15:18:38 +0300

    Feature/parse history elements error message (#444)
    * update lp history
    
    * update snapshot
    
    * add transfer recievement support
    
    * update sora-substrate
    
    * improve transfer translation
    
    * add blockHash to subquery  history, export TransactionMixin
    
    * add wip code
    
    * error message parsing
    
    * add HistoryElement type

--- src/services/types.ts
+++ src/services/types.ts
@@ -23,3 +23,52 @@ export type FiatPriceAndApyObject = {
     strategicBonusApy?: CodecString;
   };
 };
+
+export type HistoryElementError = {
+  moduleErrorId: number;
+  moduleErrorIndex: number;
+};
+
+export type HistoryElementExecution = {
+  success: boolean;
+  error?: HistoryElementError;
+};
+
+export type HistoryElementSwap = {
+  baseAssetAmount: string;
+  baseAssetId: string;
+  liquidityProviderFee: string;
+  selectedMarket: string;
+  targetAssetAmount: string;
+  targetAssetId: string;
+};
+
+export type HistoryElementTransfer = {
+  amount: string;
+  assetId: string;
+  from: string;
+  to: string;
+};
+
+export type HistoryElementLiquidityOperation = {
+  baseAssetAmount: string;
+  baseAssetId: string;
+  targetAssetAmount: string;
+  targetAssetId: string;
+  type: string;
+};
+
+export type HistoryElement = {
+  id: string;
+  blockHash: string;
+  blockHeight: string;
+  module: string;
+  method: string;
+  address: string;
+  networkFee: string;
+  execution: HistoryElementExecution;
+  timestamp: string;
+  swap: Nullable<HistoryElementSwap>;
+  transfer: Nullable<HistoryElementTransfer>;
+  liquidityOperation: Nullable<HistoryElementLiquidityOperation>;
+};
