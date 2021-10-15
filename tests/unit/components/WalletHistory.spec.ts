4200682d708cc065b1014b05a56d1b22574c23fb
RustemYuzlibaev <rustem.yuzlibaev@yandex.ru>
2021-10-15 13:39:42 +0500

    Add WalletHistory unit-test

--- tests/unit/components/WalletHistory.spec.ts
+++ tests/unit/components/WalletHistory.spec.ts
@@ -1,40 +1,51 @@
-// import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex';
+import { shallowMount } from '@vue/test-utils';
+import { Account } from '@/types/common';
+import { History } from '@sora-substrate/util';
 
-// import WalletHistory from '@/components/WalletHistory.vue'
-// import { TranslationMock, SoramitsuElementsImport } from '../../utils'
+import WalletHistory from '@/components/WalletHistory.vue';
+import { useDescribe, localVue } from '../../utils';
+import { MOCK_ACCOUNT_ASSETS, MOCK_HISTORY } from '../../utils/mock';
+import { MOCK_ACCOUNT } from '../../utils/WalletAccountMock';
 
-// TODO: fix it
-// TypeError: Cannot redefine property: configDescriptor
-//         at Function.defineProperty (<anonymous>)
-//
-//       33 | import { Component, Mixins } from 'vue-property-decorator'
-//       34 | import { Getter, Action } from 'vuex-class'
-//     > 35 | import { AccountAsset, Asset, KnownSymbols } from '@sora-substrate/util'
+const createStore = (historyArray?: Array<History> | undefined) => {
+  return new Vuex.Store({
+    getters: {
+      account: () => MOCK_ACCOUNT as Account,
+      activity: () => historyArray as Array<History>,
+      shouldBalanceBeHidden: () => true as boolean,
+    },
+    actions: {
+      getAccountActivity: jest.fn(),
+    },
+  });
+};
 
-// const localVue = createLocalVue()
+useDescribe('WalletHistory.vue', WalletHistory, () => {
+  it('should be rendered correctly', () => {
+    const wrapper = shallowMount(WalletHistory, {
+      localVue,
+      store: createStore(MOCK_HISTORY),
+    });
 
-describe('WalletBase.vue', () => {
-  beforeEach(() => {
-    // SoramitsuElementsImport(localVue)
-    // TranslationMock(WalletHistory)
+    expect(wrapper.element).toMatchSnapshot();
   });
 
-  it('should renders correctly', () => {
-    const propsData = {
-      history: [
-        {
-          operation: 'SWAP',
-          fromSymbol: 'XOR',
-          fromAmount: 100,
-          toSymbol: 'KSM',
-          toAmount: 24390.1239,
-          status: 'IN_PROGRESS',
-          date: 1605048643745,
-        },
-      ],
-    };
-    // const wrapper = shallowMount(WalletHistory, { localVue, propsData })
-    // expect(wrapper.element).toMatchSnapshot()
-    expect(true).toBe(true);
+  it('should render history empty text when no transacion', () => {
+    const wrapper = shallowMount(WalletHistory, { localVue, store: createStore() });
+    const textMessage = wrapper.find('.history-empty');
+
+    expect(textMessage.exists()).toBeTrue();
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it('asset should be readable when passed as prop', () => {
+    const wrapper = shallowMount(WalletHistory, {
+      localVue,
+      store: createStore(MOCK_HISTORY),
+      propsData: { asset: MOCK_ACCOUNT_ASSETS[0] },
+    });
+
+    expect(wrapper.vm.$props.asset).toEqual(MOCK_ACCOUNT_ASSETS[0]);
   });
 });
