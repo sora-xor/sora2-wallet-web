814a5f75a4c620a17a8fa34315988e530d8ca833
RustemYuzlibaev <rustem.yuzlibaev@yandex.ru>
2021-11-01 04:40:52 +0500

    Add wallet asset test

--- tests/unit/components/WalletAssets.spec.ts
+++ tests/unit/components/WalletAssets.spec.ts
@@ -1,53 +1,54 @@
-// import Vuex from 'vuex'
-// import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex';
+import { shallowMount } from '@vue/test-utils';
 
-// import WalletAssets from '@/components/WalletAssets.vue'
-// import { TranslationMock, SoramitsuElementsImport } from '../../utils'
+import WalletAssets from '@/components/WalletAssets.vue';
+import { AccountAsset } from '@sora-substrate/util';
+import { useDescribe, localVue } from '../../utils';
+import { MOCK_ACCOUNT_ASSETS, MOCK_FIAT_PRICE_AND_APY_OBJECT, MOCK_WALLET_PERMISSIONS } from '../../utils/mock';
+import { WalletPermissions } from '../../../src/consts';
+import type { FiatPriceAndApyObject } from '../../../src/services/types';
 
-// TODO: fix this issue
-// TypeError: Cannot redefine property: configDescriptor
-//         at Function.defineProperty (<anonymous>)
-//
-//       1 | import axiosInstance from 'axios'
-//     > 2 | import { api } from '@sora-substrate/util'
-
-// const localVue = createLocalVue()
-// localVue.use(Vuex)
-// const store = new Vuex.Store({
-//   modules: {
-//     Account: {
-//       getters: {
-//         accountAssets: () => ([
-//           {
-//             symbol: 'XOR',
-//             address: '1f9840a85d5af5bf1d1762f925bdaddc4201f984',
-//             balance: 12787.09,
-//             decimals: 18
-//           }
-//         ]),
-//         isExternal: () => false
-//       },
-//       actions: {
-//         getAccountAssets: jest.fn()
-//       }
-//     },
-//     Router: {
-//       actions: {
-//         navigate: jest.fn()
-//       }
-//     }
-//   } as any
-// })
-
-describe('WalletAssets.vue', () => {
-  beforeEach(() => {
-    // SoramitsuElementsImport(localVue)
-    // TranslationMock(WalletAssets)
+const createStore = (permissions: WalletPermissions = MOCK_WALLET_PERMISSIONS, withoutFiatAndApy = false) =>
+  new Vuex.Store({
+    getters: {
+      accountAssets: () => MOCK_ACCOUNT_ASSETS as Array<AccountAsset>,
+      fiatPriceAndApyObject: () => MOCK_FIAT_PRICE_AND_APY_OBJECT as FiatPriceAndApyObject,
+      withoutFiatAndApy: () => withoutFiatAndApy,
+      permissions: () => permissions,
+    },
   });
 
-  it('should renders correctly', () => {
-    // const wrapper = shallowMount(WalletAssets, { localVue, store })
-    // expect(wrapper.element).toMatchSnapshot()
-    expect(true).toBe(true);
+useDescribe('WalletAssets.vue', WalletAssets, () => {
+  it('should be rendered correctly', () => {
+    const wrapper = shallowMount(WalletAssets, { localVue, store: createStore() });
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it('should not render send button when sendAssets property is false', () => {
+    const wrapper = shallowMount(WalletAssets, {
+      localVue,
+      store: createStore({ ...MOCK_WALLET_PERMISSIONS, sendAssets: false }),
+    });
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it('should not render swap button when swapAssets property is false', () => {
+    const wrapper = shallowMount(WalletAssets, {
+      localVue,
+      store: createStore({ ...MOCK_WALLET_PERMISSIONS, swapAssets: false }),
+    });
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it('should not render fiat value when withoutFiatAndApy property is true', () => {
+    const wrapper = shallowMount(WalletAssets, {
+      localVue,
+      store: createStore(undefined, true),
+    });
+
+    expect(wrapper.element).toMatchSnapshot();
   });
 });
