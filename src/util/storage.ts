92f0088aabc7232b12c0c00caa3b3cd831d2d5c3
Stefan Popov <stefanpopov@list.ru>
2021-12-04 11:08:10 +0400

    Release 1.6.33 (#504)

--- src/util/storage.ts
+++ src/util/storage.ts
@@ -1,3 +1,8 @@
 import { Storage } from '@sora-substrate/util';
 
 export const storage = new Storage();
+
+/**
+ * This storage will be dependent on runtime version
+ */
+export const runtimeStorage = new Storage('runtime');
