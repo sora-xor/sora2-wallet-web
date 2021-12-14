c4c747c740493b2e074cd3472c09734f557a75ad
Stefan Popov <stefanpopov@list.ru>
2021-12-14 09:38:24 +0400

    Fix subquery fiat request (#513)
    * Fix fiat price request
    
    * Update version

--- src/services/subquery/queries/fiatPriceAndApy.ts
+++ src/services/subquery/queries/fiatPriceAndApy.ts
@@ -1,9 +1,11 @@
 export const FiatPriceQuery = `
 query PoolXYKEntities (
-  $first: Int = 1)
+  $first: Int = 1,
+  $orderBy: [PoolXykEntitiesOrderBy!] = UPDATED_DESC)
 {
   poolXYKEntities (
     first: $first
+    orderBy: $orderBy
   )
   {
     nodes {
