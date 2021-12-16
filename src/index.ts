34df4d3a03d89e5a4d3d14965966c6f40dc8335c
Nikita-Polyakov <polyakov@soramitsu.co.jp>
2021-12-16 17:21:50 +0300

    export AssetList components

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,8 @@ import installWalletPlugins from './plugins';
 import SoraWallet from './SoraWallet.vue';
 import WalletAccount from './components/WalletAccount.vue';
 import WalletAvatar from './components/WalletAvatar.vue';
+import AssetList from './components/AssetList.vue';
+import AssetListItem from './components/AssetListItem.vue';
 import InfoLine from './components/InfoLine.vue';
 import FormattedAmount from './components/FormattedAmount.vue';
 import FormattedAmountWithFiatValue from './components/FormattedAmountWithFiatValue.vue';
@@ -109,6 +111,8 @@ const components = {
   SoraWallet,
   WalletAccount,
   WalletAvatar,
+  AssetList,
+  AssetListItem,
   InfoLine,
   FormattedAmount,
   FormattedAmountWithFiatValue,
