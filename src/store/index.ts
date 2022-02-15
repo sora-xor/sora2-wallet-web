6c2b6279ab5b0691feec2e9815a4ec86f0d6ddcc
Nikita-Polyakov <polyakov@soramitsu.co.jp>
2022-02-15 09:15:55 +0300

    Revert "merge origin/develop into release/1.7.13"
    This reverts commit cf5b79d06872029e27fd4bddac38671f528ff81d, reversing
    changes made to e974d3366a181aea4eb11fe9ed98de59ee7c8962.

--- src/store/index.ts
+++ src/store/index.ts
@@ -9,53 +9,6 @@ import Subscriptions from './Subscriptions';
 
 Vue.use(Vuex);
 
-Vue.use(Vuex);
-
-const runParallel = async (actions: Array<AsyncVoidFn>) => {
-  const results = await Promise.allSettled(actions);
-
-  results.forEach((result) => {
-    if (result.status === 'rejected') {
-      console.error(result.reason);
-    }
-  });
-};
-
-const actions = {
-  // Subscriptions dependent on chain state
-  async activateNetwokSubscriptions({ dispatch }) {
-    await runParallel([
-      dispatch('subscribeOnSystemEvents'),
-      dispatch('subscribeOnRuntimeVersion'),
-      dispatch('subscribeOnAssets'),
-      dispatch('subscribeOnAccountAssets'),
-    ]);
-  },
-  async resetNetworkSubscriptions({ dispatch }) {
-    await runParallel([
-      dispatch('resetSystemEventsSubscription'),
-      dispatch('resetRuntimeVersionSubscription'),
-      dispatch('resetAssetsSubscription'),
-      dispatch('resetAccountAssetsSubscription'),
-    ]);
-  },
-  // Internal subscriptions & timers
-  async activateInternalSubscriptions({ dispatch }) {
-    await runParallel([
-      dispatch('trackActiveTransactions'),
-      dispatch('subscribeOnFiatPriceAndApyObjectUpdates'),
-      dispatch('subscribeOnExtensionAvailability'),
-    ]);
-  },
-  async resetInternalSubscriptions({ dispatch }) {
-    await runParallel([
-      dispatch('resetActiveTransactions'),
-      dispatch('resetFiatPriceAndApySubscription'),
-      dispatch('resetExtensionAvailabilitySubscription'),
-    ]);
-  },
-};
-
 const modules = {
   Account,
   Router,
@@ -67,7 +20,6 @@ const modules = {
 const store = new Vuex.Store({
   modules,
   strict: false,
-  actions,
 });
 
 export { modules };
