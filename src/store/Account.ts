beee72d1753aaecce0c25559462996ce25941605
Nikita-Polyakov <polyakov@soramitsu.co.jp>
2022-02-17 08:45:41 +0300

    wip account assets update

--- src/store/Account.ts
+++ src/store/Account.ts
@@ -51,7 +51,6 @@ const types = flow(
   map((x) => [x, x]),
   fromPairs
 )([
-  'GET_ACCOUNT_ASSETS',
   'ADD_ASSET',
   'TRANSFER',
   'IMPORT_POLKADOT_JS_ACCOUNT',
@@ -211,20 +210,6 @@ const mutations = {
     state.address = storage.get('address') || '';
     state.name = storage.get('name') || '';
     state.isExternal = Boolean(storage.get('isExternal')) || false;
-    state.accountAssets = api.assets.accountAssets; // to save reactivity
-    state.activity = api.historyList;
-  },
-
-  [types.GET_ACCOUNT_ASSETS_REQUEST](state: AccountState) {
-    state.accountAssets = [];
-  },
-
-  [types.GET_ACCOUNT_ASSETS_SUCCESS](state: AccountState, assets: Array<AccountAsset>) {
-    state.accountAssets = assets;
-  },
-
-  [types.GET_ACCOUNT_ASSETS_FAILURE](state: AccountState) {
-    state.accountAssets = [];
   },
 
   [types.UPDATE_ASSETS](state: AccountState, assets: Array<Asset>) {
@@ -339,7 +324,6 @@ const actions = {
   },
 
   async afterLogin({ dispatch }) {
-    await dispatch('getAccountAssets');
     await dispatch('subscribeOnAccountAssets');
     await dispatch('checkCurrentRoute', undefined, { root: true });
   },
@@ -439,19 +423,6 @@ const actions = {
     }
   },
 
-  async getAccountAssets({ commit, getters }) {
-    if (!getters.isLoggedIn || (api.assets.accountAssets.length && getters.accountAssets.length !== 0)) {
-      return;
-    }
-    commit(types.GET_ACCOUNT_ASSETS_REQUEST);
-    try {
-      await api.assets.getKnownAccountAssets();
-      commit(types.GET_ACCOUNT_ASSETS_SUCCESS, api.assets.accountAssets);
-    } catch (error) {
-      commit(types.GET_ACCOUNT_ASSETS_FAILURE);
-    }
-  },
-
   async getAssets({ commit, getters: { whitelist } }) {
     try {
       const assets = await api.assets.getAssets(whitelist);
@@ -484,7 +455,8 @@ const actions = {
         state.accountAssetsSubscription = api.assets.balanceUpdated.subscribe((data) => {
           commit(types.UPDATE_ACCOUNT_ASSETS, api.assets.accountAssets);
         });
-        api.assets.updateAccountAssets();
+
+        await api.assets.updateAccountAssets();
       } catch (error) {
         commit(types.UPDATE_ACCOUNT_ASSETS, []);
       }
@@ -575,12 +547,10 @@ const actions = {
   },
 
   async syncWithStorage({ commit, state, getters, dispatch }) {
-    const getAccountAssetsAddresses = () => Object.keys(getters.accountAssetsAddressTable);
-
     // previous state
     const { isLoggedIn: wasLoggedIn } = getters;
     const { address } = state;
-    const prevAddresses = getAccountAssetsAddresses();
+    const prevAddresses = Object.keys(getters.accountAssetsAddressTable);
 
     commit(types.SYNC_WITH_STORAGE);
 
@@ -595,14 +565,7 @@ const actions = {
 
     // still logged in after sync
     if (getters.isLoggedIn && wasLoggedIn) {
-      const currentAddresses = getAccountAssetsAddresses();
-      // unique addresses size of two states
-      const delta = new Set([...prevAddresses, ...currentAddresses]).size;
-
-      // if asset(s) in api.accountAssets changed, we should update subscription
-      if (prevAddresses.length !== delta || currentAddresses.length !== delta) {
-        api.assets.updateAccountAssets();
-      }
+      await api.assets.updateAccountAssets();
     }
   },
 };
