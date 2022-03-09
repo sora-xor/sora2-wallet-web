6ddd0dd2217fbb2b9da57a5d721d611d866b95b1
Nikita Polyakov <53777036+Nikita-Polyakov@users.noreply.github.com>
2022-03-09 14:52:46 +0300

    fiat prices fix (#597)

--- src/services/subquery/queries/fiatPriceAndApy.ts
+++ src/services/subquery/queries/fiatPriceAndApy.ts
@@ -1,24 +1,44 @@
 export const FiatPriceQuery = `
 query PoolXYKEntities (
   $first: Int = 1,
-  $orderBy: [PoolXykEntitiesOrderBy!] = UPDATED_DESC)
+  $orderBy: [PoolXykEntitiesOrderBy!] = UPDATED_DESC
+  $filter: PoolXYKEntityFilter
+  $poolsAfter: Cursor = ""
+  $poolsFirst: Int = 100)
 {
   poolXYKEntities (
     first: $first
     orderBy: $orderBy
+    filter: $filter
   )
   {
     nodes {
-      pools {
-        edges {
-          node {
-            targetAssetId,
-            priceUSD,
-            strategicBonusApy
-          }
+      id
+      pools (
+        first: $poolsFirst
+        after: $poolsAfter
+      ) {
+        pageInfo {
+          hasNextPage
+          endCursor
+        }
+        nodes {
+          targetAssetId,
+          priceUSD,
+          strategicBonusApy
         }
       }
     }
   }
 }
 `;
+
+export const poolXykEntityFilter = (poolXykEntityId?: string) => {
+  if (!poolXykEntityId) return undefined;
+
+  return {
+    id: {
+      equalTo: poolXykEntityId,
+    },
+  };
+};
