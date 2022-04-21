429386cd653073cad2ca642f94a743f7f6056227
RustemYuzlibaev <rustem.yuzlibaev@yandex.ru>
2022-04-21 05:35:27 +0300

    cover TokenLogo.vue with test

---
+++ tests/unit/components/TokenLogo.spec.ts
@@ -0,0 +1,54 @@
+import { useDescribe, useShallowMount, useVuex } from '../../utils';
+import TokenLogo from '@/components/TokenLogo.vue';
+import { MOCK_ACCOUNT_ASSETS, MOCK_ACCOUNT_ASSETS_NFT } from '../../utils/mock';
+
+const createStore = (mutations) =>
+  useVuex({
+    router: {
+      mutations,
+    },
+  });
+
+useDescribe('TokenLogo.vue', TokenLogo, () => {
+  it('should render correctly', () => {
+    const wrapper = useShallowMount(TokenLogo, {
+      propsData: {
+        token: MOCK_ACCOUNT_ASSETS[0],
+      },
+    });
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it('should render nft-token-logo when asset.content exists', () => {
+    const wrapper = useShallowMount(TokenLogo, {
+      propsData: {
+        token: MOCK_ACCOUNT_ASSETS_NFT[0],
+      },
+    });
+    const nftComponent = wrapper.find('.asset-logo-nft');
+
+    expect(nftComponent.exists()).toBeTrue();
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it('should call navigate() mutation when prop is passed and logo is clicked', () => {
+    const mutations = {
+      navigate: jest.fn(),
+    };
+
+    const wrapper = useShallowMount(TokenLogo, {
+      store: createStore(mutations),
+      propsData: {
+        token: MOCK_ACCOUNT_ASSETS[0],
+        withClickableLogo: true,
+      },
+    });
+
+    const element = wrapper.find('.asset-logo');
+
+    element.trigger('click');
+
+    expect(mutations.navigate).toHaveBeenCalled();
+  });
+});
