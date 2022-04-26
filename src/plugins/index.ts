7b1119491dfdc14a2d681e2a737386ee227037d2
RustemYuzlibaev <rustem.yuzlibaev@yandex.ru>
2022-04-26 12:01:51 +0500

    fix positioning

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,12 +1,10 @@
 import Vue from 'vue';
 import { install as installSoramitsuUI } from './soramitsuUI';
 import { install as installMaska } from './maska';
-import { install as installFragment } from './fragment';
 import { install as installVirtualScroller } from './virtualScroller';
 
 export default function installWalletPlugins(vue: typeof Vue, store) {
   installMaska(vue);
-  installFragment(vue);
   installVirtualScroller(vue);
   installSoramitsuUI(vue, store);
 }
