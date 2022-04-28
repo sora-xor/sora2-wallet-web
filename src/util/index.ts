9f29ff5c4a18371428291cc219f3201d15fe8a2a
Nikita Polyakov <53777036+Nikita-Polyakov@users.noreply.github.com>
2022-04-28 14:56:47 +0300

    add getScrollbarWidth util (#658)
    * add getScrollbarWidth util
    
    * fix 1px issue
    
    * add style fix
    
    * update snapshots
    
    Co-authored-by: Stefan Popov <stefanpopov@list.ru>

--- src/util/index.ts
+++ src/util/index.ts
@@ -162,3 +162,26 @@ export const getCssVariableValue = (name: string): string => {
     .getPropertyValue(name)
     .trim();
 };
+
+export const getScrollbarWidth = (): number => {
+  const outer = document.createElement('div');
+  outer.className = 'el-scrollbar__wrap';
+  outer.style.visibility = 'hidden';
+  outer.style.width = '100px';
+  outer.style.position = 'absolute';
+  outer.style.top = '-9999px';
+  document.body.appendChild(outer);
+
+  const widthNoScroll = outer.offsetWidth;
+  outer.style.overflow = 'scroll';
+
+  const inner = document.createElement('div');
+  inner.style.width = '100%';
+  outer.appendChild(inner);
+
+  const widthWithScroll = inner.offsetWidth;
+  (outer.parentNode as HTMLElement).removeChild(outer);
+  const scrollBarWidth = widthNoScroll - widthWithScroll;
+
+  return scrollBarWidth;
+};
