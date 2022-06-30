0fd9e1fa25a86567145344bb4112ed05378100c8
Nikita Polyakov <53777036+Nikita-Polyakov@users.noreply.github.com>
2022-06-30 14:12:32 +0300

    Release/1.10.0 (#697)
    * update login flow using extension as source (#684)
    
    * Feature/pw 692/drag n drop on assets (#681)
    
    * draft with sortablejs
    
    * Revert "draft with sortablejs"
    
    This reverts commit 1b08f02c11b31853ef77c0b6e181ddd0cebb69d3.
    
    * add draggable feature & return simple scrollbar
    
    * Update WalletAssets.spec.ts.snap
    
    * remove accessors & optional chaining
    
    * add check
    
    * update lib
    
    * update js-lib to 1.9.12
    
    * remove redundant update
    
    Co-authored-by: Nikita-Polyakov <polyakov@soramitsu.co.jp>
    
    * Feature/pw 507/reform history error messages (#683)
    
    * prework
    
    * modify source messages
    
    * remove unused functionality
    
    * remove comments
    
    * add more errors
    
    * add new errors
    
    * use names
    
    * Update package.json (#687)
    
    * move method to layout (#689)
    
    * move method to layout
    
    * update tests
    
    * update assets (#691)
    
    * update assets
    
    * remove logic to lib
    
    * Update WalletAssets.vue
    
    * Update package.json
    
    * Update price queries (#693)
    
    * update subquery queries
    
    * add urql
    
    * update price fetch interval
    
    * update historical price fetch method
    
    * update queries and explorer
    
    * return endpoint
    
    Co-authored-by: Rustem <rustem.yuzlibaev@yandex.ru>

--- src/services/subquery/queries/historicalPrice.ts
+++ src/services/subquery/queries/historicalPrice.ts
@@ -1,25 +1,39 @@
-export const HistoricalPriceQuery = `
-query PoolXYKEntities (
-  $orderBy: [PoolXykEntitiesOrderBy!] = UPDATED_DESC
-  $assetId: String = ""
-  $first: Int = null
-)
-{
-  poolXYKEntities (
-    orderBy: $orderBy,
-    first: $first
-  )
-  {
-    nodes {
-      pools(
-        filter: { targetAssetId: { equalTo: $assetId } }
-      ) {
-        nodes {
-          priceUSD,
-          updated
-        }
+import { gql } from '@urql/core';
+
+import { PageInfoFragment } from '../fragments/pageInfo';
+
+import { AssetSnapshotTypes } from '../types';
+
+export const HistoricalPriceQuery = gql`
+  query HistoricalPriceQuery($after: Cursor = "", $filter: AssetSnapshotFilter, $first: Int = null) {
+    assetSnapshots(after: $after, first: $first, filter: $filter, orderBy: [TIMESTAMP_DESC]) {
+      totalCount
+      pageInfo {
+        ...PageInfoFragment
+      }
+      nodes {
+        priceUSD
+        volume
+        timestamp
       }
     }
   }
-}
+  ${PageInfoFragment}
 `;
+
+export const historicalPriceFilter = (assetAddress: string, type: AssetSnapshotTypes) => {
+  return {
+    and: [
+      {
+        assetId: {
+          equalTo: assetAddress,
+        },
+      },
+      {
+        type: {
+          equalTo: type,
+        },
+      },
+    ],
+  };
+};
