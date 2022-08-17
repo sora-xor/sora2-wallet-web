d8ccbf68dda1cc8d43f1fb6feba26385bd1b2e17
RustemYuzlibaev <rustem.yuzlibaev@yandex.ru>
2022-08-17 11:46:39 +0500

    Update notification.ts

--- src/util/notification.ts
+++ src/util/notification.ts
@@ -2,15 +2,19 @@ import { WhitelistArrayItem } from '@sora-substrate/util/build/assets/types';
 import { getBase64Icon } from './image';
 
 export async function pushNotification(asset: WhitelistArrayItem, message: string): Promise<void> {
-  if (Notification.permission === 'granted') {
-    const notification = new Notification(asset.symbol, {
-      body: message,
-      icon: await getBase64Icon(asset.icon),
-    });
+  try {
+    if (Notification.permission === 'granted') {
+      const notification = new Notification(asset.symbol, {
+        body: message,
+        icon: await getBase64Icon(asset.icon),
+      });
 
-    notification.onclick = function (event) {
-      event.preventDefault(); // prevent the browser from focusing the Notification's tab
-      window.open('https://polkaswap.io/#/wallet');
-    };
+      notification.onclick = function (event) {
+        event.preventDefault(); // prevent the browser from focusing the Notification's tab
+        window.open('https://polkaswap.io/#/wallet');
+      };
+    }
+  } catch {
+    // return silently
   }
 }
