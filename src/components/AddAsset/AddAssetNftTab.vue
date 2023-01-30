ae47072ae94e1a8c4fb354b7a14d0734335998d3
Stefan Popov <17688105+stefashkaa@users.noreply.github.com>
2023-01-30 09:33:52 +0100

    Fix

--- src/components/AddAsset/AddAssetNftTab.vue
+++ src/components/AddAsset/AddAssetNftTab.vue
@@ -11,7 +11,7 @@
       />
       <asset-list :assets="foundAssets" class="asset-search-list" @click="handleSelectAsset">
         <template #list-empty>
-          {{ t(`addAsset.${assetIsAlreadyAdded ? 'alreadyAttached' : 'empty'}`) }}
+          {{ t(assetIsAlreadyAdded ? 'addAsset.alreadyAttached' : 'addAsset.empty') }}
         </template>
       </asset-list>
     </div>
