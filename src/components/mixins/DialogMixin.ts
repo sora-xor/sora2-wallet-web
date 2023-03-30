6040850479c36d13d22d851abae27302976e35d9
Stefan Popov <17688105+stefashkaa@users.noreply.github.com>
2023-03-31 01:45:33 +0200

    Prepare draft for tabindex in dialogs

--- src/components/mixins/DialogMixin.ts
+++ src/components/mixins/DialogMixin.ts
@@ -4,11 +4,14 @@ import { Vue, Component, Prop, Watch } from 'vue-property-decorator';
 export default class DialogMixin extends Vue {
   @Prop({ type: Boolean, default: false, required: true }) readonly visible!: boolean;
 
+  private nonDialogNodes: Array<HTMLElement & { prevTabindex: string | null }> = [];
   isVisible = false;
 
   @Watch('visible', { immediate: true })
   handleVisibleChange(value: boolean): void {
     this.isVisible = value;
+    if (value) this.saveItemsTabindex();
+    else this.restoreItemsTabindex();
   }
 
   @Watch('isVisible')
@@ -18,5 +21,37 @@ export default class DialogMixin extends Vue {
 
   closeDialog(): void {
     this.isVisible = false;
+  }
+
+  private saveItemsTabindex(): void {
+    const modalNodes = Array.from(document.querySelectorAll('.el-dialog *'));
+    // By only finding elements that do not have tabindex="-1" we ensure we don't
+    // corrupt the previous state of the element if a modal was already open
+    this.nonDialogNodes = Array.from(document.querySelectorAll('body *:not(.el-dialog):not([tabindex="-1"])'));
+    for (const node of this.nonDialogNodes) {
+      if (!modalNodes.includes(node)) {
+        node.prevTabindex = node.getAttribute('tabindex'); // save the previous tabindex state so we can restore it on close
+        node.setAttribute('tabindex', '-1');
+        // tabindex=-1 does not prevent the mouse from focusing the node (which
+        // would show a focus outline around the element). Prevent this by disabling
+        // outline styles while the modal is open
+        // @see https://www.sitepoint.com/when-do-elements-take-the-focus/
+        node.style.outline = 'none';
+      }
+    }
+  }
+
+  /** Restore or remove tabindex from nodes */
+  private restoreItemsTabindex(): void {
+    for (const node of this.nonDialogNodes) {
+      if (node.prevTabindex) {
+        node.setAttribute('tabindex', node.prevTabindex);
+        node.prevTabindex = null;
+      } else {
+        node.removeAttribute('tabindex');
+      }
+      node.style.outline = '';
+    }
+    this.nonDialogNodes = [];
   }
 }
