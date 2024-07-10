0f256c60ae27b45f143f893fda66d89eb8e6074c
RustemYuzlibaev <rustem.yuzlibaev@yandex.ru>
2024-07-10 11:06:32 +0300

    Update actions.ts

--- src/store/account/actions.ts
+++ src/store/account/actions.ts
@@ -260,18 +260,13 @@ const actions = defineActions({
             (asset) => !api.assets.isNftBlacklisted(asset, getters.blacklist)
           );
 
+          const sbtAssetsList = ['0x003958e97b3053835992946d797d5d5392f73005ad62ba3ce93233a519275d41'];
+
           // to know if asset is SBT or not upfront
           const assetsWithMetaInfo = filtered.map((asset) => {
-            if (whitelistAddresses.includes(asset.address)) {
-              return {
-                ...asset,
-                isSBT: false,
-              };
-            }
-
             return {
               ...asset,
-              isSBT: true, // call api.regulatedAssets.isRegulatedAsset();
+              isSBT: !!sbtAssetsList.includes(asset.address),
             };
           });
 
