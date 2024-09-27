1743f8c76a911e89a018cb2ef59ed380f9da021d
Nikita Polyakov <53777036+Nikita-Polyakov@users.noreply.github.com>
2024-09-27 10:01:18 +0300

    update indexer types (#1213)
    * update indexer types
    
    * fix import
    
    * up version

--- src/services/indexer/subquery/types/models.ts
+++ src/services/indexer/subquery/types/models.ts
@@ -1,13 +1,18 @@
 import {
   AccountBaseEntity,
+  AccountLiquidityBaseEntity,
+  AccountLiquiditySnapshotBaseEntity,
   AssetBaseEntity,
   AssetSnapshotBaseEntity,
   ConnectionQueryResponseData,
   HistoryElementBase,
   PoolXYKBaseEntity,
+  PoolSnapshotBaseEntity,
   OrderBookBaseEntity,
   OrderBookSnapshotBaseEntity,
   OrderBookOrderBaseEntity,
+  VaultBaseEntity,
+  VaultEventBaseEntity,
 } from '../../types';
 
 // Subquery Models
@@ -16,49 +21,81 @@ import {
 // with derived fields
 export type SubqueryAccountBaseEntity = AccountBaseEntity & {
   latestHistoryElementId: string;
+  lastLiquidationId: string;
 };
-export type SubqueryAccountEntity = SubqueryAccountBaseEntity & {
-  latestHistoryElement: HistoryElementBase;
-};
-
 // with derived fields
 export type SubqueryPoolXYKBaseEntity = PoolXYKBaseEntity & {
   baseAssetId: string;
   targetAssetId: string;
 };
-// with connection
-export type SubqueryPoolXYKEntity = SubqueryPoolXYKBaseEntity & {
-  baseAsset: AssetBaseEntity;
-  targetAsset: AssetBaseEntity;
+// with derived fields
+export type SubqueryAccountLiquidityBaseEntity = AccountLiquidityBaseEntity & {
+  accountId: string;
+  poolId: string;
 };
-
+// with derived fields
+export type SubqueryAccountLiquiditySnapshotBaseEntity = AccountLiquiditySnapshotBaseEntity & {
+  accountLiquidityId: string;
+};
 // with derived fields
 export type SubqueryOrderBookSnapshotBaseEntity = OrderBookSnapshotBaseEntity & {
   orderBookId: string;
 };
-// with connection
-export type SubqueryOrderBookSnapshotEntity = SubqueryOrderBookSnapshotBaseEntity & {
-  orderBook: OrderBookBaseEntity;
-};
-
 // with derived fields
 export type SubqueryOrderBookBaseEntity = OrderBookBaseEntity & {
   baseAssetId: string; // connection field
   quoteAssetId: string; // connection field
 };
-
 // with derived fields
 export type SubqueryOrderBookOrderBaseEntity = OrderBookOrderBaseEntity & {
   orderBookId: string;
   accountId: string;
 };
+// with derived fields
+export type SubqueryVaultBaseEntity = VaultBaseEntity & {
+  ownerId: string;
+  collateralAssetId: string;
+  debtAssetId: string;
+};
+// with derived fields
+export type SubqueryVaultEventBaseEntity = VaultEventBaseEntity & {
+  vaultId: string;
+};
 
+// with connection
+export type SubqueryAccountEntity = SubqueryAccountBaseEntity & {
+  latestHistoryElement: HistoryElementBase;
+  lastLiquidation: VaultEventBaseEntity;
+};
+// with connection
+export type SubqueryAccountLiquidityEntity = SubqueryAccountLiquidityBaseEntity & {
+  account: SubqueryAccountBaseEntity;
+  pool: SubqueryPoolXYKBaseEntity;
+  data: ConnectionQueryResponseData<SubqueryAccountLiquiditySnapshotBaseEntity>;
+};
+// with connection
+export type SubqueryAccountLiquiditySnapshotEntity = SubqueryAccountLiquiditySnapshotBaseEntity & {
+  accountLiquidity: SubqueryAccountLiquidityBaseEntity;
+};
+// with connection
+export type SubqueryPoolXYKEntity = SubqueryPoolXYKBaseEntity & {
+  baseAsset: AssetBaseEntity;
+  targetAsset: AssetBaseEntity;
+  data: ConnectionQueryResponseData<PoolSnapshotBaseEntity>;
+};
+// with connection
+export type SubqueryPoolSnapshotEntity = PoolSnapshotBaseEntity & {
+  pool: SubqueryPoolXYKBaseEntity;
+};
+// with connection
+export type SubqueryOrderBookSnapshotEntity = SubqueryOrderBookSnapshotBaseEntity & {
+  orderBook: OrderBookBaseEntity;
+};
 // with connection
 export type SubqueryOrderBookOrderEntity = SubqueryOrderBookOrderBaseEntity & {
   orderBook: SubqueryOrderBookBaseEntity;
   account: SubqueryAccountBaseEntity;
 };
-
 // with connection
 export type SubqueryOrderBookEntity = SubqueryOrderBookBaseEntity & {
   baseAsset: AssetBaseEntity;
@@ -66,20 +103,21 @@ export type SubqueryOrderBookEntity = SubqueryOrderBookBaseEntity & {
   data: ConnectionQueryResponseData<SubqueryOrderBookSnapshotBaseEntity>;
   orders: ConnectionQueryResponseData<SubqueryOrderBookOrderBaseEntity>;
 };
-
+// with connection
+export type SubqueryVaultEntity = SubqueryVaultBaseEntity & {
+  owner: SubqueryAccountBaseEntity;
+  collateralAsset: AssetBaseEntity;
+  debtAsset: AssetBaseEntity;
+  events: ConnectionQueryResponseData<SubqueryVaultEventBaseEntity>;
+};
+// with connection
+export type SubqueryVaultEventEntity = SubqueryVaultEventBaseEntity & {
+  vault: SubqueryVaultBaseEntity;
+};
 // with connection
 export type SubqueryAssetEntity = AssetBaseEntity & {
   data: ConnectionQueryResponseData<AssetSnapshotBaseEntity>;
   poolXYK: ConnectionQueryResponseData<SubqueryPoolXYKBaseEntity>;
-};
-
-// with derived fields
-export type SubqueryAssetSnapshotBaseEntity = AssetSnapshotBaseEntity & {
-  assetId: string;
-};
-// with connection
-export type SubqueryAssetSnapshotEntity = SubqueryAssetSnapshotBaseEntity & {
-  asset: AssetBaseEntity;
 };
 
 export type SubqueryAccountEntityMutation = {
